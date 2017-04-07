5e26aa15a712343afc22ced62499dc1b256d2149
rsukhar <ruslan@sukhar.info>
2017-04-07 15:01:50 +0300

    Initial version of the plugin #446

---
+++ functions/admin_pages.php
@@ -0,0 +1,61 @@
+<?php defined( 'ABSPATH' ) OR die( 'This script cannot be accessed directly.' );
+
+add_action( 'admin_menu', 'ogr_add_admin_pages', 30 );
+function ogr_add_admin_pages() {
+	add_submenu_page( 'tools.php', 'Optin.Guru', 'Optin.Guru', 'manage_options', 'og-settings', 'ogr_settings_page' );
+}
+
+function ogr_handle_return_to_endpoint() {
+	// Handling return to backend
+	if ( isset( $_POST['owner_id'] ) AND isset( $_POST['website_id'] ) AND isset( $_POST['token'] ) AND isset( $_POST['_nonce'] ) AND wp_verify_nonce( $_POST['_nonce'], 'ogr_connect' ) ) {
+		update_option( 'optinguru_owner_id', (int) $_POST['owner_id'], TRUE );
+		update_option( 'optinguru_website_id', (int) $_POST['website_id'], FALSE );
+		update_option( 'optinguru_token', $_POST['token'], FALSE );
+	}
+}
+
+function ogr_settings_page() {
+	$domain = 'http://optinguru.local';
+	ogr_handle_return_to_endpoint();
+	$website_id = get_option( 'optinguru_website_id' );
+	if ( $website_id === FALSE ) {
+		?>
+		<div class="ogr-connect">
+			<h1 class="ogr-connect-header">Connect Website to Optin.Guru</h1>
+			<form class="ogr-connect-card" method="post" action="<?php echo esc_attr( $domain . '/oauth2/connect_website' ) ?>">
+				<div class="ogr-connect-card-body">
+					<p>Convert more visitors into leads and subscribers using high-end responsive optin forms.</p>
+				</div>
+				<div class="ogr-connect-card-footer">
+					<input type="hidden" name="endpoint" value="<?php echo esc_attr( admin_url( 'tools.php?page=og-settings' ) ) ?>">
+					<input type="hidden" name="domain" value="<?php echo esc_attr( preg_replace( '~^https?:\/\/~', '', get_site_url() ) ) ?>">
+					<input type="hidden" name="website_name" value="<?php echo esc_attr( get_bloginfo( 'name' ) ) ?>">
+					<input type="hidden" name="platform" value="WordPress">
+					<input type="hidden" name="_nonce" value="<?php echo esc_attr( wp_create_nonce( 'ogr_connect' ) ) ?>">
+					<button class="ogr-btn action_connect">
+						Connect to Optin.Guru
+					</button>
+				</div>
+			</form>
+			<a href="https://help.optin.guru/og/connect/" class="ogr-connect-help" target="_blank">Get help connecting your site</a>
+		</div>
+		<?php
+	} else {
+		?>
+		<div class="ogr-connect type_success">
+			<h1 class="ogr-connect-header">Website is Connected to Optin.Guru</h1>
+			<div class="ogr-connect-card">
+				<div class="ogr-connect-card-body">
+					<p>Congratulations! Your website is connected to Optin.Guru.</p>
+					<p>Now you can <strong>grow your email lists easliy</strong>, using its top-notch list builder with unique features and amazing pre-built templates!</p>
+				</div>
+				<div class="ogr-connect-card-footer">
+					<a class="ogr-btn action_create" href="<?php echo esc_attr( $domain . '/widget/create?website_id=' . $website_id ) ?>">
+						Create New Optin
+					</a>
+				</div>
+			</div>
+		</div>
+		<?php
+	}
+}
--- optinguru.php
+++ optinguru.php
@@ -1,36 +1,71 @@
 <?php defined( 'ABSPATH' ) OR die( 'This script cannot be accessed directly.' );
+
 /**
- * Plugin Name: Optin.Guru
- * Version: 0.1
+ * Plugin Name: MailChimp Forms by Optin.Guru
+ * Version: 1.0.0
  * Plugin URI: https://optin.guru/
- * Description: Optin.Guru Integration
- * Author: Optin.Guru
+ * Description: Optin forms builder, compatible with MailChimp and 25+ other popular integrations.
+ * Author: CodeLights
  * Author URI: https://optin.guru/
  * License: GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: optinguru
  */
 
-add_action( 'init', 'og_init' );
-function og_init() {
-	if ( is_admin() ) {
-		return;
+// Global variables for plugin usage (global declaration is needed here for WP CLI compatibility)
+global $ogr_file, $ogr_dir, $ogr_uri, $ogr_version;
+$ogr_file = __FILE__;
+$ogr_dir = plugin_dir_path( __FILE__ );
+$ogr_uri = plugins_url( '', __FILE__ );
+$ogr_version = preg_match( '~Version\: ([^\n]+)~', file_get_contents( __FILE__, NULL, NULL, 82, 150 ), $ogr_matches ) ? $ogr_matches[1] : FALSE;
+unset( $ogr_matches );
+
+add_action( 'init', 'ogr_init' );
+function ogr_init() {
+	$owner_id = get_option( 'optinguru_owner_id' );
+	if ( ! is_admin() AND $owner_id !== FALSE ) {
+		add_action( 'wp_enqueue_scripts', 'ogr_enqueue_scripts' );
+		add_filter( 'script_loader_tag', 'ogr_script_loader_tag', 10, 2 );
 	}
-	add_action( 'wp_enqueue_scripts', 'og_enqueue_sciprts' );
-	add_filter( 'script_loader_tag', 'og_script_loader_tag', 10, 2 );
 }
 
-//add_action( 'wp_enqueue_scripts', 'og_enqueue_sciprts' );
-function og_enqueue_sciprts() {
-	// TODO Use the proper link
-	wp_enqueue_script( 'og-embed', '//app.optin.guru/hometocome/embed.js', array(), '0.1', TRUE );
+if ( is_admin() ) {
+	require $ogr_dir . 'functions/admin_pages.php';
 }
 
-//add_filter( 'script_loader_tag', 'og_script_loader_tag', 10, 2 );
-function og_script_loader_tag( $tag, $handle ) {
-	if ( $handle !== 'og-embed' ) {
+function ogr_enqueue_scripts() {
+	global $ogr_version;
+	wp_enqueue_script( 'optinguru-api', 'https://dev.optin.guru/OptinGuru.js', array(), $ogr_version, TRUE );
+}
+
+function ogr_script_loader_tag( $tag, $handle ) {
+	if ( $handle !== 'optinguru-api' ) {
 		return $tag;
 	}
+	
+	return '<script type="text/javascript" id="optinguru-api" src="https://dev.optin.guru/OptinGuru.js" data-owner="' . get_option( 'optinguru_owner_id' ) . '" async="async"></script>';
+}
 
-	return str_replace( ' src', ' async="async" id="optinguru-embed" src', $tag );
+add_action( 'admin_enqueue_scripts', 'ogr_admin_enqueue_scripts' );
+function ogr_admin_enqueue_scripts( $hook ) {
+	if ( $hook !== 'tools_page_og-settings' AND $hook !== 'admin_page_og-connect' ) {
+		return;
+	}
+
+	global $ogr_uri, $ogr_version;
+	wp_enqueue_style( 'ogr-main', $ogr_uri . '/css/main.css', array(), $ogr_version );
+	wp_enqueue_script( 'ogr-main', $ogr_uri . '/js/main.js', array( 'jquery' ), $ogr_version );
+}
+
+register_activation_hook( $ogr_file, 'ogr_activate' );
+function ogr_activate() {
+	//	header( 'Location: ' . admin_url( 'tools.php?page=og-settings' ) );
+}
+
+register_uninstall_hook( $ogr_file, 'ogr_uninstall' );
+function ogr_uninstall() {
+	// Options cleanup
+	foreach ( array( 'owner_id', 'website_id', 'token' ) as $option_name ) {
+		delete_option( 'optinguru_' . $option_name );
+	}
 }
