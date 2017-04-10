54da52234e4aa32888872dc8cff04dfe79f406ba
soulwest <sovest21@yandex.ru>
2017-04-10 18:05:09 +0300

    fix image in menu

--- functions/admin_pages.php
+++ functions/admin_pages.php
@@ -2,7 +2,11 @@
 
 add_action( 'admin_menu', 'ogr_add_admin_pages', 30 );
 function ogr_add_admin_pages() {
-	add_menu_page( 'Optin.Guru settings', 'Optin.Guru', 'manage_options', 'og-settings', 'ogr_settings_page', plugins_url( 'optinguru/images/logo_blue.png' ), 99);
+	// sorry :-(
+	echo '<style>.dashicons-admin-og{
+		background: url('.plugins_url( 'optinguru/images/og_icon_logo.png' ).') no-repeat center 5px / 30px auto !important;
+	}</style>';
+	add_menu_page( 'Optin.Guru settings', 'Optin.Guru', 'manage_options', 'og-settings', 'ogr_settings_page', 'dashicons-admin-og', 99);
 	//add_submenu_page( 'tools.php', 'Optin.Guru', 'Optin.Guru', 'manage_options', 'og-settings2', 'ogr_settings_page' );
 }
 
