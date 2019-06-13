c39a535326cc3f83385780f5a9cbc535c87e1edc
Alexander Pilipenko <webdemid@gmail.com>
2019-06-13 14:01:38 +0300

    White Label plugin for WordPress #1201

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php defined( 'ABSPATH' ) OR die( 'This script cannot be accessed directly.' );
+
+return [
+	// Logos to display in the plugin
+	'logo' => 'https://app.convertful.com/assets/img/logo_blue.png',
+	'logo@2x' => 'https://app.convertful.com/assets/img/logo_blue@2x.png',
+	// Plugin settings
+	'title' => 'Convertful',
+	'host' => 'https://app.convertful.com',
+	'script_id' => 'convertful',
+	'script_name' => 'Convertful.js',
+];
