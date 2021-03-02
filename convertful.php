c90c3b24d8fd64bea6380fda70fef3dfd7aeec91
Alex Mamaev <alexm@convertful.com>
2021-03-02 21:39:28 +0300

    WooCommerce plugin fixes

--- convertful.php
+++ convertful.php
@@ -2,10 +2,11 @@
 
 /**
  * Plugin Name: Convertful - Your Ultimate On-Site Conversion Tool
- * Version: 2.1
+ * Version: 2.1.1
  * Plugin URI: https://convertful.com/
  * Description: All the modern on-site conversion solutions, natively integrates with all modern Email Marketing Platforms.
  * Author: Convertful
+ * Author URI: https://convertful.com
  * License: GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: convertful
@@ -35,7 +36,6 @@ require $conv_dir . 'functions/shortcodes.php';
 register_uninstall_hook( $conv_file, 'conv_uninstall' );
 
 add_action( 'rest_api_init', function () {
-
 	register_rest_route( 'convertful/v2', '/complete_authorization/', [
 		'methods'  => 'POST',
 		'callback' => 'conv_complete_authorization',
@@ -49,6 +49,33 @@ add_action( 'rest_api_init', function () {
 	] );
 } );
 
+function conv_init()  {
+	global $conv_dir;
+
+	if ( get_option( 'optinguru_owner_id' ) ) {
+		update_option( 'conv_owner_id', get_option( 'optinguru_owner_id' ), TRUE );
+		update_option( 'conv_site_id', get_option( 'optinguru_site_id', get_option( 'optinguru_website_id' ) ), FALSE );
+		update_option( 'conv_token', get_option( 'optinguru_token' ) );
+		delete_option( 'optinguru_owner_id' );
+	} elseif ( get_option( 'convertful_owner_id' ) ) {
+		update_option( 'conv_owner_id', get_option( 'convertful_owner_id' ), TRUE );
+		update_option( 'conv_site_id', get_option( 'convertful_site_id' ), FALSE );
+		update_option( 'conv_token', get_option( 'convertful_token' ) );
+		delete_option( 'convertful_owner_id' );
+	}
+
+	$owner_id = get_option( 'conv_owner_id' );
+	if ( ! is_admin() and $owner_id !== FALSE ) {
+		add_action( 'wp_enqueue_scripts', 'conv_enqueue_scripts' );
+		add_filter( 'script_loader_tag', 'conv_script_loader_tag', 10, 2 );
+		add_filter( 'the_content', 'conv_after_post_content' );
+	}
+
+	if (function_exists( 'woocommerce_get_page_id' ))
+	{
+		require $conv_dir . 'functions/woocommerce.php';
+	}
+}
 /**
  * Get script id
  * @return string
@@ -71,28 +98,6 @@ function conv_get_script_filename() {
 	return conv_get_script_id() === 'convertful-api'
 		? 'Convertful.js'
 		: 'optin.js';
-}
-
-
-function conv_init() {
-	if ( get_option( 'optinguru_owner_id' ) ) {
-		update_option( 'conv_owner_id', get_option( 'optinguru_owner_id' ), TRUE );
-		update_option( 'conv_site_id', get_option( 'optinguru_site_id', get_option( 'optinguru_website_id' ) ), FALSE );
-		update_option( 'conv_token', get_option( 'optinguru_token' ) );
-		delete_option( 'optinguru_owner_id' );
-	} elseif ( get_option( 'convertful_owner_id' ) ) {
-		update_option( 'conv_owner_id', get_option( 'convertful_owner_id' ), TRUE );
-		update_option( 'conv_site_id', get_option( 'convertful_site_id' ), FALSE );
-		update_option( 'conv_token', get_option( 'convertful_token' ) );
-		delete_option( 'convertful_owner_id' );
-	}
-
-	$owner_id = get_option( 'conv_owner_id' );
-	if ( ! is_admin() and $owner_id !== FALSE ) {
-		add_action( 'wp_enqueue_scripts', 'conv_enqueue_scripts' );
-		add_filter( 'script_loader_tag', 'conv_script_loader_tag', 10, 2 );
-		add_filter( 'the_content', 'conv_after_post_content' );
-	}
 }
 
 function conv_enqueue_scripts() {
@@ -122,6 +127,7 @@ function conv_enqueue_scripts() {
 		'postType'   => get_post_type(),
 		'categories' => $categories,
 		'tags'       => $tags,
+		'ajax_url'   => get_home_url() . '/index.php?rest_route=/convertful/v2/' ,
 		'userRoles'  => ( $user_meta instanceof WP_User and ! empty( $user_meta->roles ) ) ? $user_meta->roles : [ 'guest' ],
 	] );
 }
@@ -150,7 +156,7 @@ function conv_uninstall() {
 	}
 }
 
-function conv_complete_authorization( WP_REST_Request $request ) {
+function conv_complete_authorization( $request ) {
 	$owner_id     = (int) $request->get_param( 'owner_id' );
 	$site_id      = (int) $request->get_param( 'site_id' );
 
@@ -205,6 +211,13 @@ function conv_get_info( /*WP_REST_Request $request*/ ) {
 		'post_types' => $post_types,
 		'user_roles' => $user_roles,
 	];
+
+	if (function_exists( 'woocommerce_get_page_id' )){
+		// Add WooCommerce coupons and products
+		$result['woo_coupons'] = get_woo_coupons();
+		$result['woo_products'] = get_woo_products();
+		$result['woo_enabled'] = TRUE;
+	}
 
 	wp_send_json_success( $result );
 }
---
+++ functions/woocommerce.php
@@ -0,0 +1,91 @@
+<?php defined( 'ABSPATH' ) or die( 'This script cannot be accessed directly.' );
+
+//global $woocommerce;
+
+add_action( 'rest_api_init', function () {
+	register_rest_route( 'convertful/v2', '/add_to_woo_cart/', [
+		'methods'  => 'POST',
+		'callback' => 'add_to_woo_cart',
+		'permission_callback' => '__return_true',
+	]);
+	register_rest_route( 'convertful/v2', '/add_woo_coupon/', [
+		'methods'  => 'POST',
+		'callback' => 'add_woo_coupon',
+		'permission_callback' => '__return_true',
+	]);
+});
+
+function get_woo_coupons()
+{
+	$args = array(
+		'posts_per_page'   => -1,
+		'orderby'          => 'title',
+		'order'            => 'asc',
+		'post_type'        => 'shop_coupon',
+		'post_status'      => 'publish',
+	);
+
+	$coupons = get_posts( $args );
+	$coupon_names = array();
+	foreach ( $coupons as $coupon ) {
+		// Get the name for each coupon post
+		array_push( $coupon_names, $coupon->post_title );
+	}
+
+	return $coupon_names;
+}
+
+function get_woo_products()
+{
+	$args = array(
+		'order' => 'DESC',
+	);
+	$products = wc_get_products( $args );
+	$product_names = array();
+	foreach ( $products as $product ) {
+		// Get the name for each product
+		$product_names[$product->id] = $product->name;
+	}
+
+	return $product_names;
+}
+
+function add_to_woo_cart()
+{
+	global $woocommerce;
+	$product_id = $_POST['product_id'];
+	if (! empty($product_id))
+	{
+		$woocommerce->cart->add_to_cart($product_id);
+	}
+}
+
+function add_woo_coupon(){
+	$coupon_code = $_POST['code']; // Code
+	$amount = $_POST['amount'];
+	$discount_type = $_POST['discount_type'];
+	$emails = $_POST['emails'];
+	$coupon = array(
+		'post_title' => $coupon_code,
+		'post_excerpt' => 'Convertful auto-generated coupon',
+		'post_content' => '',
+		'post_status' => 'publish',
+		'post_author' => 1,
+		'post_type' => 'shop_coupon'
+	);
+	$new_coupon_id = wp_insert_post( $coupon );
+	update_post_meta( $new_coupon_id, 'discount_type', $discount_type );
+	update_post_meta( $new_coupon_id, 'coupon_amount', $amount );
+	update_post_meta( $new_coupon_id, 'individual_use', 'yes' );
+	update_post_meta( $new_coupon_id, 'product_ids', '' );
+	update_post_meta( $new_coupon_id, 'exclude_product_ids', '' );
+	update_post_meta( $new_coupon_id, 'exclude_sale_items', 'yes' );
+	update_post_meta( $new_coupon_id, 'usage_limit', 1 );
+	update_post_meta( $new_coupon_id, 'expiry_date', '' );
+	update_post_meta( $new_coupon_id, 'free_shipping', 'no' );
+	if (! empty($emails))
+	{
+		$emails = (array)$emails;
+		update_post_meta( $new_coupon_id, 'customer_email', $emails );
+	}
+}
