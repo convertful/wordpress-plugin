22a8271e6a9c604386e2fc779ac52f1e880dea2c
Alex Mamaev <alexm@convertful.com>
2021-03-04 17:20:15 +0300

    WooCommerce plugin fixes pt.3

--- convertful.php
+++ convertful.php
@@ -2,7 +2,7 @@
 
 /**
  * Plugin Name: Convertful - Your Ultimate On-Site Conversion Tool
- * Version: 2.1.1
+ * Version: 2.2
  * Plugin URI: https://convertful.com/
  * Description: All the modern on-site conversion solutions, natively integrates with all modern Email Marketing Platforms.
  * Author: Convertful
